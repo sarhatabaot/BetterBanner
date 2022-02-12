a3f408667197c1ce2f43c3a9dfd88f4abf84f40f
Omer Oreg <sarhatabaot1@gmail.com>
2022-02-12 18:42:56 +0200

    Using an immutable list

--- src/main/java/com/netrust/betterbanner/BannerUtil.java
+++ src/main/java/com/netrust/betterbanner/BannerUtil.java
@@ -1,20 +1,42 @@
 package com.netrust.betterbanner;
 
 
+import com.google.common.collect.ImmutableList;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author sarhatabaot
  */
 public class BannerUtil {
-    private static List<Material> banners = createBannerList();
-    public static boolean isBannerInInventory(Inventory inventory){
-        for (Material material: banners){
-            if(inventory.contains(material))
+    private static final ImmutableList<Material> banners = ImmutableList.of(
+            Material.BLACK_BANNER,
+            Material.BLUE_BANNER,
+            Material.BROWN_BANNER,
+            Material.CYAN_BANNER,
+            Material.GRAY_BANNER,
+            Material.GREEN_BANNER,
+            Material.LIGHT_BLUE_BANNER,
+            Material.LIGHT_GRAY_BANNER,
+            Material.LIME_BANNER,
+            Material.MAGENTA_BANNER,
+            Material.ORANGE_BANNER,
+            Material.PINK_BANNER,
+            Material.PURPLE_BANNER,
+            Material.RED_BANNER,
+            Material.WHITE_BANNER,
+            Material.YELLOW_BANNER
+    );
+
+    private BannerUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    public static boolean isBannerInInventory(Inventory inventory) {
+        for (Material material : banners) {
+            if (inventory.contains(material))
                 return true;
         }
         return false;
@@ -23,26 +45,4 @@ public class BannerUtil {
     public static boolean isBanner(Material material) {
         return banners.contains(material);
     }
-
-    private static List<Material> createBannerList(){
-        ArrayList<Material> list = new ArrayList<Material>();
-        list.add(Material.BLACK_BANNER);
-        list.add(Material.BLUE_BANNER);
-        list.add(Material.BROWN_BANNER);
-        list.add(Material.CYAN_BANNER);
-        list.add(Material.GRAY_BANNER);
-        list.add(Material.GREEN_BANNER);
-        list.add(Material.LIGHT_BLUE_BANNER);
-        list.add(Material.LIGHT_GRAY_BANNER);
-        list.add(Material.LIME_BANNER);
-        list.add(Material.MAGENTA_BANNER);
-        list.add(Material.ORANGE_BANNER);
-        list.add(Material.PINK_BANNER);
-        list.add(Material.PURPLE_BANNER);
-        list.add(Material.RED_BANNER);
-        list.add(Material.WHITE_BANNER);
-        list.add(Material.YELLOW_BANNER);
-        return list;
-    }
-
 }
